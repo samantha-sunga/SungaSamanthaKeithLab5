0c16084ee3a14c20c0e428f65e2511da8de98180
joshuajumawan <rommeljoshua.jumawan.iics@ust.edu.ph>
2018-10-08 16:37:24 +0800

    Initial commit

---
+++ app/src/main/java/lab5/samanthasunga/com/sungasamanthakeithlab5/Main2Activity.java
@@ -0,0 +1,32 @@
+package lab5.samanthasunga.com.sungasamanthakeithlab5;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+
+public class Main2Activity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        Log.d("4ITF ", "oncreate has executed.... ");
+
+    }
+    public void process(View v) {
+        Intent i = null, chooser = null;
+        if (v.getId() == R.id.btnNext) {
+            i = new Intent(Intent.ACTION_VIEW);
+            i.setData(Uri.parse("geo:14.578428, 120.983472"));
+            chooser = Intent.createChooser(i, "Select your map app");
+
+            startActivity(chooser);
+        }
+    }
+    public void display2(View v) {
+        Intent i = new Intent(this, MainActivity.class);
+        startActivity(i);
+    }
+}
---
+++ app/src/main/java/lab5/samanthasunga/com/sungasamanthakeithlab5/MainActivity.java
@@ -0,0 +1,73 @@
+package lab5.samanthasunga.com.sungasamanthakeithlab5;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d("4ITF ", "On create has executed....");
+
+        Intent i =  new Intent(this, MyService.class);
+        startService(i);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("4ITF ", "On start has executed....");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("4ITF ", "On resume has executed....");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("4ITF ", "On pause has executed....");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("4ITF ", "On stop has executed....");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d("4ITF ", "On Restart has executed....");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("4ITF ", "The object has been deallocated...");
+    }
+    public void display2(View v) {
+        Intent i = new Intent(this, Main2Activity.class);
+        startActivity(i);
+    }
+
+    public void process(View v) {
+        Intent i = null, chooser = null;
+        if (v.getId() == R.id.btnNext) {
+            i = new Intent(Intent.ACTION_VIEW);
+            i.setData(Uri.parse("geo:14.586915, 120.981192"));
+            chooser = Intent.createChooser(i, "Select your map app");
+
+            startActivity(chooser);
+        }
+    }
+}
+
